767e29db5d072c8943d786f61f087b0da4d37681
wisdomugo <wisdomjudeson@gmail.com>
2019-10-04 12:27:38 +0100

    hiding contents with the hidden attribute binding CORRECT

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,12 +8,12 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
     <div>Time: {{event?.time}}</div>
     <div>Price: \${{event?.price}}</div>
     
-    <div *ngIf="event?.location">  <!-- this line hidden with ngIf which just comments it out -->
+    <div [hidden]="!event?.location">
         <span>Location: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}}, {{event.location?.country}}</span>
     </div>
 
-    <div *ngIf="event?.onlineUrl"> <!-- this line hidden with ngIf which just comments it out -->
+    <div [hidden]="!event?.onlineUrl">
         Online URL: {{event?.onlineUrl}}
     </div>
     </div>
