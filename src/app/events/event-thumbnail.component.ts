69374641b2a84e54241da43c6a1f6556c58bbfe5
wisdomugo <wisdomjudeson@gmail.com>
2019-10-04 07:56:25 +0100

    Communicating parent component with hardcoded data

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
     selector: 'event-thumbnail',
     template: `
@@ -12,6 +12,7 @@ import {Component, Input} from '@angular/core';
         <span>&nbsp;</span>
         <span>{{event.location.city}}, {{event.location.country}}</span>
     </div>
+    <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
     </div>
     `
 
@@ -19,4 +20,10 @@ import {Component, Input} from '@angular/core';
 
 export class EventThumbnailComponent {
     @Input() event:any
+    @Output() eventClick = new EventEmitter()
+
+    handleClickMe(){
+        //console.log('clicked!');
+        this.eventClick.emit(this.event.name)
+    }
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core'
     <div>
         <h1>Upcoming Angular Events</h1>
         <hr>
-        <event-thumbnail [event]="event1" ></event-thumbnail>
+        <event-thumbnail [event]="event1" (eventClick) = "handleEventClicked($event)" ></event-thumbnail>
     </div>
     `
 })
@@ -23,6 +23,10 @@ export class EventsListComponent {
             city: 'London',
             country: 'England'
         }
+    }
+
+    handleEventClicked(data) {
+        console.log('received:', data)
     }
 
 }
