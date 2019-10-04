e0adae3aed1f251cc880f746b390bea22ad6d9e6
wisdomugo <wisdomjudeson@gmail.com>
2019-10-04 13:26:38 +0100

    styling components with ngClass and class binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,12 +1,20 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
+import { getRenderedText } from '@angular/core/src/render3';
 @Component({
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    
-    <div [ngSwitch]="event?.time">
+
+    <!-- This is a comment showing how to use class binding to style component 
+        [class.green]="event?.time === '8:00 am'"-->
+
+    <!-- This shows how to apply css using ngClass but inline, it makes the code cumbersome to read so we
+    rather applied it by calling a function
+    [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" -->
+
+    <div [ngClass]="getStartTimeClass()"  [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -28,6 +36,8 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
     .pad-left {margin-left:10px;}
     .well div {color: #bbb; }
     .thumbnail{min-height: 210px;}
+    .green { color: #003300 !important;}
+    .bold {font-weight: bold;}
     `]
 
 })
@@ -38,5 +48,12 @@ export class EventThumbnailComponent {
 
     logFoo(){
         console.log('foo')
+    }
+
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am'){
+            return 'green bold'
+            return ''
+        }
     }
 }
