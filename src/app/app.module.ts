0230fd6d4ba919b327f5f9d60bc9bca27b4fb1b4
wisdomugo <wisdomjudeson@gmail.com>
2019-10-04 09:23:20 +0100

    adding site header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component'
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
  
   providers: [],
