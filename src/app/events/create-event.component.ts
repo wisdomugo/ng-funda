56dfb4e5a871c82bcc9b365fa8240aef922db49a
wisdomugo <wisdomjudeson@gmail.com>
2019-10-08 17:43:40 +0100

    editing data with two way binding

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -16,11 +16,30 @@ import { EventService } from './shared/index'
 })
 
 export class CreateEventComponent {
-    newEvent
+
     isDirty:boolean = true
+    event
+
     constructor(private router:Router, private eventService:EventService){ 
 
     }
+
+    ngOnInit(){
+        this.event = {
+            name: 'Ng Spectacular',
+            date: '8/8/2028',
+            time: '10 am',
+            price: 799.99,
+            location: {
+                address: '456 Happy St',
+                city: 'Felicity',
+                country: 'Angularistan'
+            },
+            onlineUrl: 'http://ngSpectacular.com',
+            imageUrl: 'http://ngSpectacular.com/logo.png'
+        }
+    }
+
     saveEvent(formValues){
         this.eventService.saveEvent(formValues)
         this.isDirty = false
