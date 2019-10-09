7a38906d15b66b2931bc31a302df9d3b24a12f45
wisdomugo <wisdomjudeson@gmail.com>
2019-10-09 14:17:08 +0100

    creating a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,8 @@ import{
   EventRouteActivator,
   EventListResolver,
   CreateSessionComponent,
-  SessionListComponent
+  SessionListComponent,
+  DurationPipe
 }from './events/index'
 
 import { EventsAppComponent } from './events-app.component';
@@ -46,7 +47,8 @@ import { Error404Component } from './errors/404.component'
     Error404Component,
     CreateSessionComponent,
     SessionListComponent,
-    CollapsibleWellComponent
+    CollapsibleWellComponent,
+    DurationPipe
   ],
  
   providers: [
