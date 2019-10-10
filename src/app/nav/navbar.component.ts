666bc09295e0ce4dd8e94f69dda0fdd92827b89f
wisdomugo <wisdomjudeson@gmail.com>
2019-10-10 20:23:45 +0100

    created the trigger directgive WORKING AT THIS STAGE NOW

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import {AuthService} from '../user/auth.service'
 import {ISession} from '../events/shared/event.model'
 import {EventService} from '../events/index'
-//import { $ } from 'protractor';
 
 @Component({
     selector: 'nav-bar',
@@ -23,12 +22,11 @@ export class NavBarComponent {
     constructor(private auth:AuthService, private eventService: EventService){
 
     }
-
     searchSessions(searchTerm){
         this.eventService.searchSessions(searchTerm).subscribe
         (sessions => {
             this.foundSessions = sessions;
-            console.log(this.foundSessions);
+            
         })
     }
 
