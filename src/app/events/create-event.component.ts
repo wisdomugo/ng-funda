2253d6dbc9e86ca47cbf47b659defdcf4c4cf42b
wisdomugo <wisdomjudeson@gmail.com>
2019-10-11 18:09:03 +0100

    using POST and PUT

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -26,10 +26,12 @@ export class CreateEventComponent {
 
 
     saveEvent(formValues){
-        this.eventService.saveEvent(formValues)
-        this.isDirty = false
-        this.router.navigate(['/events'])
+        this.eventService.saveEvent(formValues).subscribe(() => {
+            this.isDirty = false
+            this.router.navigate(['/events'])
+        });
     }
+
     cancel(){
         this.router.navigate(['/events'])
     }
