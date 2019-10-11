85a354ee498096c10a8b54a7111f76461885529f
wisdomugo <wisdomjudeson@gmail.com>
2019-10-11 16:35:52 +0100

    moving data storage to the server

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,14 +1,24 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import {Observable, Subject } from 'rxjs'
+import {Observable, of, Subject } from 'rxjs'
 import {IEvent, ISession} from './event.model'
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class EventService{
+
+  constructor(private http: HttpClient){
+
+  }
     getEvents():Observable<IEvent[]>{
-      let subject = new Subject<IEvent[]>()
-      setTimeout(() => {subject.next(EVENTS); subject.complete();},
-      100)
-        return subject
+      return this.http.get<IEvent[]>('/api/events')
+      .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])))
+    }
+
+    private handleError<T> (operation = 'operation', result?: T){
+      return (error: any): Observable<T> => {
+        console.error(error);  return of(result as T);
+      }
     }
 
     getEvent(id:number):IEvent{
