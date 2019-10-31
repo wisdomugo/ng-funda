70c584ea7752b6b73886d26cc2b66529f51d32d8
wisdom ugo <wisdomjudeson@gmail.com>
2019-10-31 11:48:23 -0700

    testing components with deep integrated tests

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,6 +1,9 @@
 import {TestBed, async, ComponentFixture} from '@angular/core/testing'
 import {DebugElement} from '@angular/core'
 import {SessionListComponent} from './session-list.component'
+import {UpvoteComponent} from './upvote.component'
+import {DurationPipe} from '../shared/duration.pipe'
+import {CollapsibleWellComponent} from '../../common/collapsible-well.component'
 import {AuthService} from '../../user/auth.service'
 import {VoterService} from './voter.service'
 import {ISession} from '../shared/event.model'
@@ -18,7 +21,10 @@ describe('SessionListComponent', () => {
         TestBed.configureTestingModule({
             imports: [],
             declarations: [
-                SessionListComponent
+                SessionListComponent,
+                UpvoteComponent,
+                CollapsibleWellComponent,
+                DurationPipe
             ],
             providers: [
                 {provide: AuthService, useValue: mockAuthService},
@@ -33,5 +39,23 @@ describe('SessionListComponent', () => {
         component = fixture.componentInstance;
         debugEl = fixture.debugElement;
         element = fixture.nativeElement;
+    })
+
+    describe('initial display', () => {
+        it('should have the correct session title', () => {
+            component.sessions = [{id: 3, name: 'Session 1', presenter: 'Joe', duration: 1,
+                    level: 'beginner', abstract: 'abstract', voters: ['john', 'bob']}];
+            
+            component.filterBy =  'all';
+            component.sortBy = 'name';
+            component.eventId = 4;
+
+            component.ngOnChanges();
+            fixture.detectChanges();
+
+            expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+
+
+        })
     })
 })
